322af154daa99d57c30629600b5f5f7d37f7b6ff
Kober <pedrokoberstain2@gmail.com>
2024-02-28 20:27:50 -0400

    Iniciando o microsserviço

---
+++ src/test/java/com/compassuol/challenge3/Notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassuol.challenge3.Notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
