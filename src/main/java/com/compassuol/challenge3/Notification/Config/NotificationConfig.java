7cc67671d67febdbc422fd5373057913d04ae76c
Kober <pedrokoberstain2@gmail.com>
2024-02-29 01:47:21 -0400

    feat: Implementando o Listener que ira receber as mensagens

---
+++ src/main/java/com/compassuol/challenge3/Notification/Config/NotificationConfig.java
@@ -0,0 +1,14 @@
+package com.compassuol.challenge3.Notification.Config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationConfig {
+
+    @RabbitListener(queues = "${mq.queues.emissao-notification}")
+    public void recieveMessage(String payload) {
+        System.out.println("Received <" + payload + ">");
+    }
+}
